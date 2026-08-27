b1e223ea9562714b1a8a9f40450a0024bfdad272
Marcel Jar <marcel.jar@senecapolytechnic.ca>
2026-08-27 13:47:51 -0400

    Changed source codes for fundamentals_i

--- fundamentals_i/immutables.py
+++ fundamentals_i/immutables.py
@@ -1,6 +1,16 @@
-x = 10     # x is an object of type int with a value of 10
-y = x      # y points to the same object as x (see id(x))
-y = 20     # y gets assigned a new int object with a value of 20
+x = 10     # x is an int object with a value of 10
+print("After x = 10")
+print("id(x):", id(x))
+
+y = x      # y points to the same object as x 
+print("After y = x")
+print("id(x):", id(x))
+print("id(y):", id(y))
+
+y = 20     # y gets assigned a new int object 
+print("After y = 20")
+print("id(x):", id(x))
+print("id(y):", id(y))
 
 print("The value of x is: ", x)
 print("The value of y is: ", y)
--- fundamentals_i/mutables.py
+++ fundamentals_i/mutables.py
@@ -1,6 +1,16 @@
-list1 = [3, 4, 2, 1]  #list1 points to a list object
-list2 = list1         #list2 points to the same list object
-list2.append(6)       #object pointed by list1 and list2 changes
+list1 = [3, 4, 2, 1]  # list1 points to a list object
+print("After list1 = [3, 4, 2, 1]")
+print("id(list1):", id(list1))
+
+list2 = list1         # list2 points to the same list object
+print("After list2 = list1")
+print("id(list1):", id(list1))
+print("id(list2):", id(list2))
+
+list2.append(6)  # objects pointed by both lists change
+print("After list2.append(6)")
+print("id(list1):", id(list1))
+print("id(list2):", id(list2))
 
 print("list1 contains: ", list1)
 print("list2 contains: ", list2)
